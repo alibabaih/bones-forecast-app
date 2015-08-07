6a63f9684862f4c59704af5d12e111840aba7478
alibabaih <alibabaih@gmail.com>
2015-08-07 15:49:41 +0400

    Routing, Templates and Controllers

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.config(function($routeProvider) {
+
+    $routeProvider
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
     
-    $scope.name = 'Vasiliy';
+});
+
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+    
+    $scope.name = 'Main';
     
 }]);
 
-myApp.controller('secondController', ['$scope', function($scope) {
-    
-    $scope.name = 'Ivan';
+
+myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
+    $scope.name = 'Second';
     
 }]);
