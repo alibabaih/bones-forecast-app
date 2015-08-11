c64f875644bd3797161474364a85c46b60abee72
alibabaih <alibabaih@gmail.com>
2015-08-11 14:53:23 +0400

    Understanding Compile and Link

--- app.js
+++ app.js
@@ -23,7 +23,35 @@ myApp.config(function ($routeProvider) {
 
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
     
+    $scope.people = [
+        {
+            name: 'John Doe',
+            address: '555 Main St.',
+            city: 'New York',
+            state: 'NY',
+            zip: '11111'
+        },
+        {
+            name: 'Jane Doe',
+            address: '333 Second St.',
+            city: 'New York',
+            state: 'NY',
+            zip: '11111'
+        },
+        {
+            name: 'Vico Doe',
+            address: '777 Third St.',
+            city: 'New York',
+            state: 'NY',
+            zip: '11111'
+        }
+                    ]
     
+    $scope.formattedAddress = function(person) {
+      
+        return person.address + ', ' + person.city + ', ' + person.state + ' ' + person.zip;
+        
+    };
     
 }]);
 
@@ -37,6 +65,16 @@ myApp.directive("searchResult", function() {
    return {
        restrict: 'AECM',
        templateUrl: 'directives/searchresult.html',
-       replace: true
+       replace: true,
+       scope: {
+           personObject: "=",
+           formattedAddressFunction: "&"
+       }, link: function(scope, elements, attrs) {
+           console.log('Compiling...');
+           if(scope.personObject.name == 'Jane Doe') {
+               elements.removeAttr('class');
+           }
+           console.log(elements);
+       }
    }
 });
